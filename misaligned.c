adffd8f6bdb7703b9a8d53830e6ac60ad26bda67
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-01-23 17:16:05 +0530

    misaligned.c file decomposed and tested

--- misaligned.c
+++ misaligned.c
@@ -1,20 +1,57 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+void printColorMapOnConsole(const char*,const char*,int,int);
+
+const char* getMajorcolor(int);
+const char* getMinorcolor(int);
+
+const char* getMajorcolor(int index)
+{
+	return majorColor[index];
+}
+
+const char* getMinorcolor(int index)
+{
+	return minorColor[index];
+}
+
+int printColorMap(void (*fun_printonConsole)(const char*, const char*,int,int), const char* (*fun_getMajorMinorcolor[])(int)) {
+	int i=0,j=0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            fun_printonConsole(fun_getMajorMinorcolor[0](i), 
+            					   fun_getMajorMinorcolor[1](i),i,j);
         }
     }
     return i * j;
 }
 
+
+void printColorMapOnConsole(const char* colorMajor,const char* colorMinor, int index_major, int index_minor)
+{
+	printf("%d | %s | %s\n", index_major* 5 + index_minor,colorMajor,colorMinor); 
+}
+
+void Test_printColorMapOnConsole(const char* colorMajor,const char* colorMinor,int index_major, int index_minor)
+{
+	assert(colorMajor == majorColor[index_major]);
+	assert(colorMinor == minorColor[index_minor]);
+}
+
 int main() {
-    int result = printColorMap();
+	const char* (*l_getMajorMinorcolor[])(int) = {getMajorcolor,getMinorcolor}; 
+	void (*l_printonConsole)(const char*, const char*,int,int) = printColorMapOnConsole;	
+    int result = printColorMap(l_printonConsole, l_getMajorMinorcolor);
+    assert(result == 25);
+
+    /* Testing function printColorMap */
+    void (*l_testprintonConsole)(const char*, const char*,int,int) = Test_printColorMapOnConsole;	
+    result = printColorMap(l_testprintonConsole, l_getMajorMinorcolor);
     assert(result == 25);
     printf("All is well (maybe!)\n");
     return 0;
