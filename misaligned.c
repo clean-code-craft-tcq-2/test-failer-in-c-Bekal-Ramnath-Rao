a4702a028287bf2e67cc6f389f4d168a9ff13d31
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2022-02-01 07:44:09 +0530

    Checking misalignment

--- misaligned.c
+++ misaligned.c
@@ -13,7 +13,39 @@
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-void printColorMapOnConsole(const char*,const char*,int,int);
+#if(ENVIRONMENT == ENVIRONMENT_TEST)
+
+int Test_index=0;
+
+char Test_Buffer[25][25] =   {" 0 | White  | Blue",
+							  " 1 | White  | Orange",
+							  " 2 | White  | Green",
+							  " 3 | White  | Brown",
+							  " 4 | White  | Slate",
+							  " 5 | Red    | Blue",
+							  " 6 | Red    | Orange",
+							  " 7 | Red    | Green",
+							  " 8 | Red    | Brown",
+							  " 9 | Red    | Slate",
+							  "10 | Black  | Blue",
+							  "11 | Black  | Orange",
+							  "12 | Black  | Green",
+							  "13 | Black  | Brown",
+							  "14 | Black  | Slate",
+							  "15 | Yellow | Blue",
+							  "16 | Yellow | Orange",
+							  "17 | Yellow | Green",
+							  "18 | Yellow | Brown",
+							  "19 | Yellow | Slate",
+							  "20 | Violet | Blue",
+							  "21 | Violet | Orange",
+							  "22 | Violet | Green",
+							  "23 | Violet | Brown",
+							  "24 | Violet | Slate"
+							  };
+#endif
+
+char buffer[50];
 
 const char* getMajorcolor(int);
 const char* getMinorcolor(int);
@@ -28,28 +60,34 @@ const char* getMinorcolor(int index)
 	return minorColor[index];
 }
 
-int printColorMap(void (*fun_printonConsole)(const char*, const char*,int,int), const char* (*fun_getMajorMinorcolor[])(int)) {
+int printColorMap(void (*fun_formatonConsole)(const char*, const char*,int,int, void(*)()), const char* (*fun_getMajorMinorcolor[])(int)
+					,void (*fun_printOnConsole)()) {
 	int i=0,j=0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            fun_printonConsole(fun_getMajorMinorcolor[MAJOR_COLOR](i), 
-            					   fun_getMajorMinorcolor[MINOR_COLOR](i),i,j);
+            fun_formatonConsole(fun_getMajorMinorcolor[MAJOR_COLOR](i), 
+            					   fun_getMajorMinorcolor[MINOR_COLOR](i),i,j,fun_printOnConsole);
         }
     }
     return i * j;
 }
 
-
-void printColorMapOnConsole(const char* colorMajor,const char* colorMinor, int index_major, int index_minor)
+void printColorMapOnConsole()
 {
-	printf("%d | %s | %s\n", index_major* 5 + index_minor,colorMajor,colorMinor); 
+	printf("%s",buffer);
+}
+
+void formatColorMapOnConsole(const char* colorMajor,const char* colorMinor, int index_major, int index_minor,void (*fun_printOnConsole)())
+{
+	sprintf(buffer,"%d | %s | %s\n", index_major* 5 + index_minor,colorMajor,colorMinor); 
+	fun_printOnConsole();
 }
 
 #if(ENVIRONMENT == ENVIRONMENT_TEST)
-void Test_printColorMapOnConsole(const char* colorMajor,const char* colorMinor,int index_major, int index_minor)
+void Test_PrintandFormatOnConsole()
 {
-	assert(colorMajor == majorColor[index_major]);
-	assert(colorMinor == minorColor[index_minor]);
+	assert(buffer == Test_Buffer[Test_index]);
+	Test_index++;
 }
 
 const char* Test_getMajorcolor(int index)
@@ -61,18 +99,10 @@ const char* Test_getMinorcolor(int index)
 {
 	return getMinorcolor(index);
 }
-#endif
 
-int main() {
-	const char* (*l_getMajorMinorcolor[])(int) = {getMajorcolor,getMinorcolor}; 
-	void (*l_printonConsole)(const char*, const char*,int,int) = printColorMapOnConsole;	
-    int result = printColorMap(l_printonConsole, l_getMajorMinorcolor);
-    assert(result == 25);
-
-	#if(ENVIRONMENT == ENVIRONMENT_TEST)
-    /* Testing function printColorMap, getMinorcolor, getMajorcolor */
-    void (*l_testprintonConsole)(const char*, const char*,int,int) = Test_printColorMapOnConsole;
-    const char* l_testgetMajorcolor = Test_getMajorcolor(0);
+void Test_Environment()
+{
+	const char* l_testgetMajorcolor = Test_getMajorcolor(0);
     assert(strcmp(l_testgetMajorcolor ,"White") == 0);
     l_testgetMajorcolor = Test_getMajorcolor(3);
     assert(strcmp(l_testgetMajorcolor ,"Yellow") == 0);
@@ -80,9 +110,30 @@ int main() {
     assert(strcmp(l_testgetMinorcolor ,"Orange") == 0);
     l_testgetMinorcolor = Test_getMinorcolor(2);
     assert(strcmp(l_testgetMinorcolor ,"Green")  == 0);
-    result = printColorMap(l_testprintonConsole, l_getMajorMinorcolor);
+
+	/* Testing function printColorMap, getMinorcolor, getMajorcolor, formatColorMapOnConsole */
+	const char* (*l_testgetMajorMinorcolor[])(int) = {getMajorcolor,getMinorcolor}; 
+    void (*l_testformatonConsole)(const char*, const char*,int,int,void (*)()) = formatColorMapOnConsole;
+    void (*l_testprintOnConsole)() = Test_PrintandFormatOnConsole;
+    int result = printColorMap(l_testformatonConsole, l_testgetMajorMinorcolor,l_testprintOnConsole);
     assert(result == 25);
+
     printf("All is well (maybe!)\n");
+}
+#endif
+
+int main() {
+
+	#if(ENVIRONMENT == ENVIRONMENT_PRODUCTION)
+	const char* (*l_getMajorMinorcolor[])(int) = {getMajorcolor,getMinorcolor}; 
+	void (*l_formatonConsole)(const char*, const char*,int,int,void (*p)()) = formatColorMapOnConsole;
+	void (*l_printOnConsole)() = printColorMapOnConsole;
+    int result = printColorMap(l_formatonConsole, l_getMajorMinorcolor,l_printOnConsole);
+    assert(result == 25);
+    #endif
+    
+	#if(ENVIRONMENT == ENVIRONMENT_TEST)
+    Test_Environment();
     #endif
     return 0;
 }
